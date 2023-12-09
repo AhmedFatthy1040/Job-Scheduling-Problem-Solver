5b1d4d6bc2b398d67e1e91b74440a711e064b287
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2023-12-09 15:04:15 +0200

    Testing the backtracking algorithm.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from models.resource import Resource
 from models.job import Job
 from models.job_scheduling_problem import JobSchedulingProblem
+from algorithms.backtracking_algorithm import BacktrackingAlgorithm
 
+# Define the job scheduling problem instance
 jobs = [
     Job(1, 3),
-    Job(2, 5, 1),
-    Job(3, 2, 2),
+    Job(2, 1, dependency=1),
+    Job(3, 4),
 ]
 
 resources = [
@@ -15,4 +17,6 @@ resources = [
 
 jsp = JobSchedulingProblem(jobs, resources)
 
-jsp.display_problem()
+# Create an instance of the BacktrackingAlgorithm and solve the problem
+backtracking_algorithm = BacktrackingAlgorithm(jsp)
+backtracking_algorithm.solve()
